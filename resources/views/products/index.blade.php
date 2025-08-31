ff85d3ed0e8bca0e1e108e0614f44a7e08ab4722
Olakiitan <ekundayoopeyemi123@gmail.com>
2025-08-31 17:41:14 +0100

    refactor: restructure product index view for clarity

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,67 +2,82 @@
 
 @section('content')
 
- <main class="container">
-        <section>
-            <div class="titlebar">
-                <h1>Products</h1>
-                <a href="{{ route('products.create') }}" class="btn-link">Add Product</a>
+<main class="container">
+    <section>
+        <div class="titlebar">
+            <h1>Products</h1>
+            <a href="{{ route('products.create') }}" class="btn-link">Add Product</a>
+        </div>
+
+        @if ($message = Session::get('success'))
+
+        @endif
+        <div class="table">
+            <div class="table-filter">
+                <div>
+                    <ul class="table-filter-list">
+                        <li>
+                            <p class="table-filter-link link-active">All</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="table">
-                <div class="table-filter">
-                    <div>
-                        <ul class="table-filter-list">
-                            <li>
-                                <p class="table-filter-link link-active">All</p>
-                            </li>
-                        </ul>
-                    </div>
+            <div class="table-search">
+                <div>
+                    <button class="search-select">
+                       Search Product
+                    </button>
+                    <span class="search-select-arrow">
+                        <i class="fas fa-caret-down"></i>
+                    </span>
                 </div>
-                <div class="table-search">
-                    <div>
-                        <button class="search-select">
-                           Search Product
-                        </button>
-                        <span class="search-select-arrow">
-                            <i class="fas fa-caret-down"></i>
-                        </span>
-                    </div>
-                    <div class="relative">
-                        <input class="search-input" type="text" name="search" placeholder="Search product..." value="{{ request('search') }}">
-                    </div>
+                <div class="relative">
+                    <input class="search-input" type="text" name="search" placeholder="Search product..." value="{{ request('search') }}">
                 </div>
-                <div class="table-product-head">
-                    <p>Image</p>
-                    <p>Name</p>
-                    <p>Category</p>
-                    <p>Inventory</p>
-                    <p>Actions</p>
-                </div>
+            </div>
+            <div class="table-product-head">
+                <p>Image</p>
+                <p>Name</p>
+                <p>Description</p>
+                <p>Quantity</p>
+                <p>Price</p>
+                <p>Actions</p>
+            </div>
+
+            <!-- Loop through actual products -->
+            @forelse($products as $product)
                 <div class="table-product-body">
-                    <img src="1.jpg"/>
-                    <p> Product name</p>
-                    <p>Category</p>
-                    <p>Inventory</p>
+                    <img src="{{ asset('images/' . $product->image) }}" alt="{{ $product->name }}" width="50"/>
+                    <p>{{ $product->name }}</p>
+                    <p>{{ $product->description ?? 'N/A' }}</p>
+                    <p>{{ $product->quantity }}</p>
+                    <p>${{ number_format($product->price, 2) }}</p>
                     <div>
-                        <button class="btn btn-success" >
-                            <i class="fas fa-pencil-alt" ></i>
+                        <button class="btn btn-success">
+                            <i class="fas fa-pencil-alt"></i>
                         </button>
-                        <button class="btn btn-danger" >
+                        <button class="btn btn-danger">
                             <i class="far fa-trash-alt"></i>
                         </button>
                     </div>
                 </div>
-                <div class="table-paginate">
-                    <div class="pagination">
-                        <a href="#" disabled>&laquo;</a>
-                        <a class="active-page">1</a>
-                        <a>2</a>
-                        <a>3</a>
-                        <a href="#">&raquo;</a>
-                    </div>
+            @empty
+                <div class="table-product-body">
+                    <p colspan="6">No products found.</p>
+                </div>
+            @endforelse
+
+            <div class="table-paginate">
+                <div class="pagination">
+                    <a href="#" disabled>&laquo;</a>
+                    <a class="active-page">1</a>
+                    <a>2</a>
+                    <a>3</a>
+                    <a href="#">&raquo;</a>
                 </div>
             </div>
-        </section>
- </main>
+        </div>
+    </section>
+</main>
 
 @endsection
