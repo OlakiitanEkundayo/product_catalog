62001b6fedd9b800d6dee7c512b0d61072cff1e4
Olakiitan <ekundayoopeyemi123@gmail.com>
2025-09-04 22:07:21 +0100

    feat: display pagination links in product index view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,103 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-
-<main class="container">
-    <section>
-        <div class="titlebar">
-            <h1>Products</h1>
-            <a href="{{ route('products.create') }}" class="btn-link">Add Product</a>
-        </div>
-
-        @if ($message = Session::get('success'))
-
-        <script type="text/javascript">
-
-            const Toast = Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-                toast.onmouseenter = Swal.stopTimer;
-                toast.onmouseleave = Swal.resumeTimer;
-            }
-            });
-            Toast.fire({
-            icon: "success",
-            title: "Signed in successfully"
-            });
-
-        </script>
-
-        @endif
-        <div class="table">
-            <div class="table-filter">
-                <div>
-                    <ul class="table-filter-list">
-                        <li>
-                            <p class="table-filter-link link-active">All</p>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="table-search">
-                <div>
-                    <button class="search-select">
-                       Search Product
-                    </button>
-                    <span class="search-select-arrow">
-                        <i class="fas fa-caret-down"></i>
-                    </span>
-                </div>
-                <div class="relative">
-                    <input class="search-input" type="text" name="search" placeholder="Search product..." value="{{ request('search') }}">
-                </div>
-            </div>
-            <div class="table-product-head">
-                <p>Image</p>
-                <p>Name</p>
-                <p>Description</p>
-                <p>Quantity</p>
-                <p>Price</p>
-                <p>Actions</p>
+    <main class="container">
+        <section>
+            <div class="titlebar">
+                <h1>Products</h1>
+                <a href="{{ route('products.create') }}" class="btn-link">Add Product</a>
             </div>
 
-            <!-- Loop through actual products -->
-            @forelse($products as $product)
-                <div class="table-product-body">
-                    <img src="{{ asset('images/' . $product->image) }}" alt="{{ $product->name }}" width="50"/>
-                    <p>{{ $product->name }}</p>
-                    <p>{{ $product->description ?? 'N/A' }}</p>
-                    <p>{{ $product->quantity }}</p>
-                    <p>${{ number_format($product->price, 2) }}</p>
+            @if ($message = Session::get('success'))
+            @endif
+            <div class="table">
+                <div class="table-filter">
                     <div>
-                        <button class="btn btn-success">
-                            <i class="fas fa-pencil-alt"></i>
-                        </button>
-                        <button class="btn btn-danger">
-                            <i class="far fa-trash-alt"></i>
-                        </button>
+                        <ul class="table-filter-list">
+                            <li>
+                                <p class="table-filter-link link-active">All</p>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            @empty
-                <div class="table-product-body">
-                    <p colspan="6">No products found.</p>
-                </div>
-            @endforelse
+                <form action="{{ route('products.index') }}" method="get" accept-charset="UTF-8" role="search">
+                    <div class="table-search">
+                        <div>
+                            <button class="search-select">
+                                Search Product
+                            </button>
+                            <span class="search-select-arrow">
+                                <i class="fas fa-caret-down"></i>
+                            </span>
+                        </div>
+                        <div class="relative">
+                            <input class="search-input" type="text" name="search" placeholder="Search product..."
+                                value="{{ request('search') }}">
+                        </div>
+                    </div>
+                </form>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($products as $product)
+                            <tr>
+                                <td>
+                                    <img src="{{ asset('images/' . $product->image) }}" alt="{{ $product->name }}"
+                                        class="img-product" />
+                                </td>
+                                <td>{{ $product->name }}</td>
+                                <td>{{ $product->description ?? 'N/A' }}</td>
+                                <td>{{ $product->quantity }}</td>
+                                <td>${{ number_format($product->price, 2) }}</td>
+                                <td>
+                                    <button class="btn btn-success">
+                                        <i class="fas fa-pencil-alt"></i>
+                                    </button>
+                                    <button class="btn btn-danger">
+                                        <i class="far fa-trash-alt"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="6">No products found.</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
 
-            <div class="table-paginate">
-                <div class="pagination">
-                    <a href="#" disabled>&laquo;</a>
-                    <a class="active-page">1</a>
-                    <a>2</a>
-                    <a>3</a>
-                    <a href="#">&raquo;</a>
+
+                <div class="table-paginate">
+                {{ $products->links('layouts.pagination') }}
+
                 </div>
+
             </div>
-        </div>
-    </section>
-</main>
-
+        </section>
+    </main>
 @endsection
