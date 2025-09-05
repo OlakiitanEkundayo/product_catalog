bb41f62c9108b5523305b19c543144e44044ae0a
Olakiitan <ekundayoopeyemi123@gmail.com>
2025-09-05 20:35:44 +0100

    chore: include SweetAlert2 CDN in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,28 @@
 <body>
 
     @yield('content')
+    @if(session('success'))
+<script>
+    Swal.fire({
+        title: 'Success!',
+        text: "{{ session('success') }}",
+        icon: 'success',
+        confirmButtonText: 'OK'
+    });
+</script>
+@endif
+
+@if(session('error'))
+<script>
+    Swal.fire({
+        title: 'Error!',
+        text: "{{ session('error') }}",
+        icon: 'error',
+        confirmButtonText: 'OK'
+    });
+</script>
+@endif
+
 
 
 </body>
